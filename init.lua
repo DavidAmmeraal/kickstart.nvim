c4928670eb98c7b2d9f03a386c7a32af32706c02
David <david@macbookpro-1.home>
2024-11-07 21:32:39 +0100

    remove unused theme

--- init.lua
+++ init.lua
@@ -876,7 +876,7 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
@@ -884,6 +884,7 @@ require('lazy').setup({
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'tokyonight-night'
       vim.cmd.colorscheme 'gruvbox'
+      vim.o.background = 'dark' -- or "light" for light mode
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
