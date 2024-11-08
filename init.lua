54d12016023161793567adcf96b8ba2de8a22196
David <david@macbookpro-1.home>
2024-11-08 14:09:54 +0100

    use catpuccin

--- init.lua
+++ init.lua
@@ -876,18 +876,22 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'ellisonleao/gruvbox.nvim',
+    'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
+    lazy = false,
+    config = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.colorscheme 'gruvbox'
-      vim.o.background = 'dark' -- or "light" for light mode
+      -- vim.o.background = 'dark' -- or "light" for light mode
+      require('catppuccin').setup {
+        flavour = 'macchiato',
+      }
+      vim.cmd.colorscheme 'catppuccin'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 
