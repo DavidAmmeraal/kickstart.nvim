65364d94a1730843ff4b64cac71d790593a5f5a0
DavidAmmeraal <d.ammeraal@quin.md>
2024-11-14 09:28:52 +0100

    put nvim tree in a floating window

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -12,7 +12,6 @@ local function my_on_attach(bufnr)
   vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts 'Up')
   vim.keymap.set('n', '<leader>tt', api.tree.toggle, { desc = '[T]oggle [T]ree' })
 end
-
 local function open_nvim_tree()
   -- open the tree
   require('nvim-tree.api').tree.open()
@@ -30,6 +29,9 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
     end
   end,
 })
+
+local HEIGHT_RATIO = 0.8 -- You can change this
+local WIDTH_RATIO = 0.5 -- You can change this too
 
 return {
   {
@@ -50,7 +52,30 @@ return {
         },
         update_focused_file = { enable = true },
         view = {
-          width = 30,
+          float = {
+            enable = true,
+            open_win_config = function()
+              local screen_w = vim.opt.columns:get()
+              local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+              local window_w = screen_w * WIDTH_RATIO
+              local window_h = screen_h * HEIGHT_RATIO
+              local window_w_int = math.floor(window_w)
+              local window_h_int = math.floor(window_h)
+              local center_x = (screen_w - window_w) / 2
+              local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+              return {
+                border = 'rounded',
+                relative = 'editor',
+                row = center_y,
+                col = center_x,
+                width = window_w_int,
+                height = window_h_int,
+              }
+            end,
+          },
+          width = function()
+            return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+          end,
         },
       }
     end,
