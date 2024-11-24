5ef361f55cd95e49fb9268b81850506e458b3583
DavidAmmeraal <david.ammeraal@gmail.com>
2024-11-24 21:12:31 +0100

    add git menu

--- init.lua
+++ init.lua
@@ -326,6 +326,7 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
+        { '<leader>g', group = '[G]it' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
     },
