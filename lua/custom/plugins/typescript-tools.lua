129acd18b37a7cf6afe9970734e33416d26a87a2
DavidAmmeraal <david.ammeraal@gmail.com>
2024-12-14 16:51:04 +0100

    configure typescript tools to prefer non-relative imports

--- lua/custom/plugins/typescript-tools.lua
+++ lua/custom/plugins/typescript-tools.lua
@@ -2,4 +2,14 @@ return {
   'pmizio/typescript-tools.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
   opts = {},
+  config = function()
+    require('typescript-tools').setup {
+      settings = {
+        tsserver_file_preferences = {
+          importModuleSpecifierPreference = 'non-relative',
+          importModuleSpecifierEnding = 'minimal',
+        },
+      },
+    }
+  end,
 }
